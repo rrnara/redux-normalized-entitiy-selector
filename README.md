a286563c99a816b332449fe686ad1aaf1ee581c8
Ram Ramani <rrnara@hotmail.com>
2020-10-12 17:44:36 -0400

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # redux-normalized-entitiy-selector
 Enables selecting normalized entities as class with functions from Redux State
+
+Use this along with normalizr (https://github.com/paularmstrong/normalizr) so that you can use this selector to pick entities in your redux store, where the entities are returned as Class objects with functions to select other related entities
+
+## Example
+
+```js
+// SchemaDefinition.js
+import ormGenerator, { NormalizrSchema } from 'redux-normalized-entitiy-selector'
+
+const user = new NormalizrSchema.Entity('users')
+const todo = new schema.Entity('todos')
+const file = new schema.Entity('files')
+const tag = new schema.Entity('tags')
+
+
+user.define({
+  files: [file],
+  todos: [todo]
+})
+
+todo.define({
+  user
+})
+
+file.define({
+  user,
+  tags: [tag]
+})
+
+tag.define({
+  files: [file]
+})
+
+const Schema = {
+  user,
+  todo,
+  file,
+  tag
+}
+
+class User {
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+class File {
+  hasTags() {
+    return this.tags().length > 0
+  }
+}
+
+const classExtensions = {
+  user: User,
+  file: File
+}
+
+// 'appEntities' is the redux store where normalized entities are stored for the app
+const { Selectors: sel, Classes: cla, PropTypes: pt } = ormGenerator(Schemas, classExtensions, 'appEntities')
+
+export { sel as default }
+export const Classes = cla
+export const PropTypes = pt
+
+// Usage
+import Selectors from 'SchemaDefinition.js'
+
+const mapStateToProps = (state, props) => {
+  const user = Selectors.user(state, state.client.current_user)
+  const tags = Selectors.tags(state, null) // This will return all tags in the redux state
+  const files = Selectors.files(state, [1,'2',3]) // This will return files which have the normalized key (identifier) 1,2,3. String or number values
+  const todos = user.todos() // This will return all todos which have been assigned to user
+  const tagsEmpty = Selectors.tags(state, null, true) // If true is third parameter - null will be assumed as empty array instead of all keys in the entity redux state
+  return {
+    user,
+    tags,
+    files,
+    todos,
+    tagsEmpty
+  }
+}
+```
---
+++ src/index.js
@@ -0,0 +1,118 @@
+import { map, findKey, compact, keys } from 'lodash';
+import { schema } from 'normalizr';
+
+for (let key of ['Entity', 'Array', 'Object', 'Union', 'Values']) {
+  schema[key].prototype.getType = () => key;
+}
+export const NormalizrSchema = schema;
+
+function proptypesFor(classes) {
+  return map(classes, function (klass) {
+    return function (props, propName, componentName) {
+      if (!(props[propName] instanceof klass)) {
+        return new Error('Invalid prop `' + propName + '` supplied to `' + componentName + '`. Validation failed.');
+      }
+      return undefined;
+    };
+  });
+}
+
+class DefaultEntityBase {}
+
+export default function ormGenerator(normalizrSchema, classExtensions, reduxStore = 'entities') {
+  const selectors = {};
+  const entityClasses = {};
+  Object.keys(normalizrSchema).forEach(function (entityName) {
+    const entity = normalizrSchema[entityName];
+    const entityTableName = normalizrSchema[entityName].key;
+    const associations = entity.schema;
+    function assiciationType(key) {
+      return Array.isArray(associations[key]) ? 'Array' : associations[key].getType ? associations[key].getType() : null;
+    }
+
+    var associationNamesToEntityNames = {};
+
+    Object.keys(associations).forEach(name => {
+      const type = assiciationType(name);
+      // If this association refers to another single object type
+      if (type === 'Entity') {
+        // Find the entity name of the association, since name may not use same string as entity name
+        var assocSchemaName = findKey(normalizrSchema, obj => obj === associations[name]);
+        associationNamesToEntityNames[name] = assocSchemaName;
+      } else if (type === 'Array') {
+        // Find assocSchemaArrayName since name may not use same string as entities name
+        var assocSchemaArrayName = associations[name][0]._key;
+        associationNamesToEntityNames[name] = assocSchemaArrayName;
+      } // Ignoring objects or other schema types
+    });
+
+    var associationNames = Object.keys(associations || {});
+
+    const EntityBase = classExtensions[entityName] || DefaultEntityBase;
+    class EntityClass extends EntityBase {
+      #state;
+      #attr;
+
+      constructor(state, attrs) {
+        super();
+        this.#state = state;
+        this.#attr = attrs;
+
+        for (const key of Object.keys(attrs)) {
+          // set the modified id names for the keys
+          if (associationNames.includes(key)) {
+            const type = assiciationType(key);
+            if (type === 'Entity') {
+              this[key + 'Id'] = attrs[key];
+            } else if (type === 'Array') {
+              this[key + 'Ids'] = attrs[key];
+            } else {
+              console.error('unknown key type');
+            }
+          } else {
+            this[key] = attrs[key];
+          }
+        }
+      }
+
+      getState() {
+        return this.#state;
+      }
+
+      getAttr() {
+        return this.#attr;
+      }
+    }
+
+    associationNames.forEach(function (name) {
+      function selectObject() {
+        return selectors[associationNamesToEntityNames[name]](this.getState(), this.getAttr()[name]);
+      }
+      EntityClass.prototype[name] = selectObject;
+    });
+
+    entityClasses[entityName] = EntityClass;
+    selectors[entityName] = function (state, id) {
+      // Check existance
+      if (!state[reduxStore][entityTableName] || !state[reduxStore][entityTableName][id]) {
+        return null;
+      }
+      return new entityClasses[entityName](state, state[reduxStore][entityTableName][id]);
+    };
+
+    selectors[entityTableName] = function (state, idArray, undefinedIsEmpty = false) {
+      // Check params
+      if (!state[reduxStore][entityTableName]) {
+        return [];
+      }
+      if (idArray == null) {
+        idArray = undefinedIsEmpty ? [] : keys(state[reduxStore][entityTableName]);
+      }
+      // Return array of selected objects, must pass in an array or will select everything from state
+      return compact(idArray.map(id => selectors[entityName](state, id)));
+    };
+  });
+
+  const PropTypes = proptypesFor(entityClasses);
+  return { Selectors: selectors, Classes: entityClasses, PropTypes: PropTypes };
+}
