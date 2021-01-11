b37fb946fc93fc5070251d7b3517cb2da05ffcda
Ram Ramani <rrnara@hotmail.com>
2021-01-11 13:55:40 -0800

    Fix handling union value not present

--- src/index.js
+++ src/index.js
@@ -17,108 +17,110 @@ export default function ormGenerator(normalizrSchema, classExtensions, reduxStor
     const associations = entity.schema;
     const EntityBase = classExtensions[entityName] || DefaultEntityBase;
     const entityType = entity.getType()
-    if (entityType !== 'Entity') {
-      if (entityType === 'Union') {
-        entityClasses[entityName] = class extends EntityBase {
-          static getEntityType(value) {
-            return entity._schemaAttribute(value);
-          }
-        }
-      }
-      return;
-    }
 
     function assiciationType(key) {
       return Array.isArray(associations[key]) ? 'Array' : associations[key].getType ? associations[key].getType() : null;
     }
 
-    var associationNamesToEntityNames = {};
+    if (entityType === 'Union') {
+      entityClasses[entityName] = class extends EntityBase {
+        static getEntityType(value) {
+          return entity._schemaAttribute(value);
+        }
+      }
+    } else if (entityType === 'Entity') {
 
-    Object.keys(associations).forEach(name => {
-      const type = assiciationType(name);
-      // If this association refers to another single object type
-      if (type === 'Entity' || type === 'Union') {
-        // Find the entity name of the association, since name may not use same string as entity name
-        var assocSchemaName = findKey(normalizrSchema, obj => obj === associations[name]);
-        associationNamesToEntityNames[name] = assocSchemaName;
-      } else if (type === 'Array') {
-        // Find assocSchemaArrayName since name may not use same string as entities name
-        var assocSchemaArrayName = associations[name][0]._key;
-        associationNamesToEntityNames[name] = assocSchemaArrayName;
-      } // Ignoring objects or other schema types
-    });
+      var associationNamesToEntityNames = {};
+  
+      Object.keys(associations).forEach(name => {
+        const type = assiciationType(name);
+        // If this association refers to another single object type
+        if (type === 'Entity' || type === 'Union') {
+          // Find the entity name of the association, since name may not use same string as entity name
+          var assocSchemaName = findKey(normalizrSchema, obj => obj === associations[name]);
+          associationNamesToEntityNames[name] = assocSchemaName;
+        } else if (type === 'Array') {
+          // Find assocSchemaArrayName since name may not use same string as entities name
+          var assocSchemaArrayName = associations[name][0]._key;
+          associationNamesToEntityNames[name] = assocSchemaArrayName;
+        } // Ignoring objects or other schema types
+      });
+  
+      var associationNames = Object.keys(associations || {});
 
-    var associationNames = Object.keys(associations || {});
+      entityClasses[entityName] = class extends EntityBase {
+        #state;
+        #attr;
 
-    entityClasses[entityName] = class extends EntityBase {
-      #state;
-      #attr;
+        constructor(state, attrs) {
+          super();
+          this.#state = state;
+          this.#attr = attrs;
 
-      constructor(state, attrs) {
-        super();
-        this.#state = state;
-        this.#attr = attrs;
-
-        for (const key of Object.keys(attrs)) {
-          // set the modified id names for the keys
-          if (associationNames.includes(key)) {
-            const type = assiciationType(key);
-            if (type === 'Entity' || type === 'Union') {
-              this[key + 'Id'] = attrs[key];
-            } else if (type === 'Array') {
-              this[key + 'Ids'] = attrs[key];
+          for (const key of Object.keys(attrs)) {
+            // set the modified id names for the keys
+            if (associationNames.includes(key)) {
+              const type = assiciationType(key);
+              if (type === 'Entity' || type === 'Union') {
+                this[key + 'Id'] = attrs[key];
+              } else if (type === 'Array') {
+                this[key + 'Ids'] = attrs[key];
+              } else {
+                console.error(`unknown key type: ${key}`);
+              }
             } else {
-              console.error(`unknown key type: ${key}`);
+              this[key] = attrs[key];
             }
-          } else {
-            this[key] = attrs[key];
           }
         }
-      }
 
-      getEntityName() {
-        return entityName;
-      }
-
-      getState() {
-        return this.#state;
-      }
-
-      getAttr() {
-        return this.#attr;
-      }
-    }
-
-    associationNames.forEach(function (name) {
-      function selectObject() {
-        const attribute = this.getAttr()[name]
-        if (get(attribute,'schema') && attribute.id) {
-          return selectors[attribute.schema](this.getState(), attribute.id, true); //undefinedIsEmpty is true
+        getEntityName() {
+          return entityName;
         }
-        return selectors[associationNamesToEntityNames[name]](this.getState(), attribute, true); //undefinedIsEmpty is true
-      }
-      entityClasses[entityName].prototype[name] = selectObject;
-    });
 
-    selectors[entityName] = function (state, id) {
-      // Check existance
-      if (!state[reduxStore][entityTableName] || !state[reduxStore][entityTableName][id]) {
-        return null;
-      }
-      return new entityClasses[entityName](state, state[reduxStore][entityTableName][id]);
-    };
+        getState() {
+          return this.#state;
+        }
 
-    selectors[entityTableName] = function (state, idArray, undefinedIsEmpty = false) {
-      // Check params
-      if (!state[reduxStore][entityTableName]) {
-        return [];
-      }
-      if (idArray == null) {
-        idArray = undefinedIsEmpty ? [] : keys(state[reduxStore][entityTableName]);
-      }
-      // Return array of selected objects, must pass in an array or will select everything from state
-      return compact(idArray.map(id => selectors[entityName](state, id)));
-    };
+        getAttr() {
+          return this.#attr;
+        }
+      } 
+
+      associationNames.forEach(function (name) {
+        function selectObject() {
+          const attribute = this.getAttr()[name]
+          if (get(attribute,'schema') && attribute.id) {
+            return selectors[attribute.schema](this.getState(), attribute.id, true); //undefinedIsEmpty is true
+          }
+          if (selectors[associationNamesToEntityNames[name]] != null) {
+            return selectors[associationNamesToEntityNames[name]](this.getState(), attribute, true); //undefinedIsEmpty is true
+          }
+          return null
+        }
+        entityClasses[entityName].prototype[name] = selectObject;
+      });
+
+      selectors[entityName] = function (state, id) {
+        // Check existance
+        if (!state[reduxStore][entityTableName] || !state[reduxStore][entityTableName][id]) {
+          return null;
+        }
+        return new entityClasses[entityName](state, state[reduxStore][entityTableName][id]);
+      };
+  
+      selectors[entityTableName] = function (state, idArray, undefinedIsEmpty = false) {
+        // Check params
+        if (!state[reduxStore][entityTableName]) {
+          return [];
+        }
+        if (idArray == null) {
+          idArray = undefinedIsEmpty ? [] : keys(state[reduxStore][entityTableName]);
+        }
+        // Return array of selected objects, must pass in an array or will select everything from state
+        return compact(idArray.map(id => selectors[entityName](state, id)));
+      };
+    }
   });
 
   const PropTypes = mapValues(entityClasses, (klass, entityName) => {
